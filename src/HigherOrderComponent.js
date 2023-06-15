0b67a59723675d9029a28d3a17ca4ffebdfa7891
Никитюк В.А <NikitukVA@kuzlitmash.org>
2023-06-15 15:19:36 +0300

    HOC with sliders

--- src/HigherOrderComponent.js
+++ src/HigherOrderComponent.js
@@ -18,21 +18,26 @@ import './App.css';
 //     } 
 // }
 
-  
+// оборачивание комп-та в обёртку с доп поведением
 const withSlider=(BaseComponent, getData)=>{
-    return (props)=>{
+    return (props)=>{ // из props приходит объект
         const [slide, setSlide] = useState(0);
         const [autoplay, setAutoplay] = useState(false);
 
-    useEffect(() => {
-        setSlide(getData());
-    }, [])
+        useEffect(() => {
+            setSlide(getData());
+        }, [])
 
-    function changeSlide(i) {
-        setSlide(slide => slide + i);
-    }
+        function changeSlide(i) {
+            setSlide(slide => slide + i);
+        }
 
-    return <BaseComponent/>
+        return <BaseComponent
+            {...props}  // отдельнве св-ва комп-та
+            slide={slide} 
+            autoplay={autoplay} 
+            changeSlide={changeSlide} 
+            setAutoplay={setAutoplay}/>
     }
 }
 
@@ -40,73 +45,82 @@ const withSlider=(BaseComponent, getData)=>{
 const getDataFromFirstFetch = () => {return 10};
 const getDataFromSecondFetch = () => {return 20};
 
-const SliderFirst = () => {
-    const [slide, setSlide] = useState(0);
+const SliderFirst = (props) => {
 
-    useEffect(() => {
-        setSlide(getDataFromFirstFetch());
-    }, [])
-
-    function changeSlide(i) {
-        setSlide(slide => slide + i);
-    }
+    console.log(props.name);
 
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide}</div>
+                <div className="text-center mt-5">Active slide {props.slide}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
+                        onClick={() => props.changeSlide(-1)}>-1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
+                        onClick={() => props.changeSlide(1)}>+1</button>
                 </div>
             </div>
         </Container>
     )
 }
 
-const SliderSecond = () => {
-    const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoplay] = useState(false)
-
-    useEffect(() => {
-        setSlide(getDataFromSecondFetch());
-    }, [])
-
-    function changeSlide(i) {
-        setSlide(slide => slide + i);
-    }
+const SliderSecond = (props) => {
+   
 
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
+                <div className="text-center mt-5">Active slide {props.slide} <br/>{props.autoplay ? 'auto' : null} </div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
+                        onClick={() => props.changeSlide(-1)}>-1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
+                        onClick={() => props.changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => setAutoplay(autoplay => !autoplay)}>toggle autoplay</button>
+                        onClick={() => props.setAutoplay(autoplay => !autoplay)}>toggle autoplay</button>
                 </div>
             </div>
         </Container>
     )
 }
+
+
+const SliderWithFirstFetch=withSlider(SliderFirst, getDataFromFirstFetch );
+const SliderWithSecondFetch=withSlider(SliderSecond, getDataFromSecondFetch);
+
+//hoc withLogger, в кот приходит арг-т WrappedComponent, 
+const withLogger=(WrappedComponent)=> props =>{
+    useEffect(()=>{
+        console.log('first render withLogger');
+    }, []);
+
+    return <WrappedComponent {...props}/>
+}
+// как компонент WithLogger, но сокращ форма
+
+const Hello=()=>{
+    return(
+        <h1>
+            Hello guys u suck
+        </h1>
+    )
+}
+
+const HelloWithLogger=withLogger(Hello);
 
 function App() {
     return (
         <>
-            <SliderFirst/>
-            <SliderSecond/>
+            <HelloWithLogger  />
+            <SliderWithFirstFetch name={'name is Vladzikini'}/>
+            <SliderWithSecondFetch />
         </>
     );
 }
